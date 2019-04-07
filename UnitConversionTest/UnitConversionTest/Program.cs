9aa06b3b95dd70225bc5d70684901863bbc0044c
Peter Centellini <peter.centellini@redesajn.com>
2019-04-07 18:45:18 +0200

    Added UnitConversionTest project

---
+++ UnitConversionTest/UnitConversionTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConversionTest {
+  class Program {
+    static void Main(string[] args) {
+
+      UnitConverter unitConverter = new UnitConverter();
+
+      foreach (string s in File.ReadAllLines("Conversions.txt")) {
+        if (s.IndexOf("?") == -1)
+          unitConverter.ParseConverterDefinition(s);
+        else
+          Console.WriteLine(unitConverter.Convert(s));
+      } // foreach
+    }
+  }
+}
