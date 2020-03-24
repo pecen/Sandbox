0ba1f464c14888ebf32290c6c7987c11daea2141
Peter Centellini <peter.centellini@scania.com>
2020-03-24 11:57:03 +0100

    Added GridSelectRowTest project

---
+++ GridSelectRowTest/GridSelectRowTest/Entry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridSelectRowTest {
+  public class Entry : INotifyPropertyChanged {
+    private bool _isChecked;
+
+    public bool IsChecked {
+      get => _isChecked;
+      set {
+        if (value == _isChecked) return;
+        _isChecked = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+}
