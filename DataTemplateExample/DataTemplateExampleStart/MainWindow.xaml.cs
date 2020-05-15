a3120397b5008f369e8c2776d84dcf967d4d1b3e
Peter Centellini <peter.c@trivec.se>
2020-05-15 19:26:10 +0200

    Added ControlTemplateExample and DataTemplateExample

---
+++ DataTemplateExample/DataTemplateExampleStart/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace DataTemplateExampleStart {
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window {
+    SampleData sdata = new SampleData();
+
+    public MainWindow() {
+      InitializeComponent();
+      DataContext = this;
+      
+      // Add this line to the Window element instead of this.DataContext here...
+      // DataContext="{Binding RelativeSource={RelativeSource Self}}" 
+
+    }
+
+    public ObservableCollection<Employee> Employees { get { return sdata.EmployeeList; } }
+  }
+}
---
+++ DataTemplateExample/DataTemplateExampleStart/SampleData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+
+namespace DataTemplateExampleStart {
+  public class SampleData {
+    public ObservableCollection<Employee> EmployeeList = new ObservableCollection<Employee>();
+
+    public SampleData() {
+      EmployeeList.Add(new Employee() { EmployeeName = "Mark Long" });
+      EmployeeList.Add(new Employee() { EmployeeName = "Frank Hampton" });
+      EmployeeList.Add(new Employee() { EmployeeName = "Jessica Chambly" });
+      EmployeeList.Add(new Employee() { EmployeeName = "Lauren Nicolette" });
+      EmployeeList.Add(new Employee() { EmployeeName = "Janet Roberts" });
+
+    }
+  }
+}
