b5f1ea61cffedc2ace4da13e24412b1313c7be23
Peter Centellini <peter.c@trivec.se>
2020-05-18 17:48:03 +0200

    DataTemplateExample: Some refactoring.

--- DataTemplateExample/DataTemplateExampleStart/Employee.cs
+++ DataTemplateExample/DataTemplateExampleStart/Employee.cs
@@ -4,19 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataTemplateExampleStart
-{
-    public class Employee
-    {
-        public string EmployeeName { get; set; }
+namespace DataTemplateExampleStart {
+  public class Employee {
+    public string EmployeeName { get; set; }
 
-        // Instead of overriding ToString()
-        // use the DisplayMemberPath property to set a path to the value
-        //
-        public override string ToString()
-        {
-            return EmployeeName;
-        }
-
+    // Instead of overriding ToString()
+    // use the DisplayMemberPath property to set a path to the value
+    // by adding to the Xaml code what you want do display,
+    // for instance DisplayMemberPath="Name"
+    public override string ToString() {
+      return EmployeeName;
     }
+
+  }
 }
