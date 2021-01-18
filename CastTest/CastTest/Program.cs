a255f082dfd63636de2450f2a74839e741e911b1
503212497 <peter.centellini@cytiva.com>
2021-01-18 10:42:22 +0100

    Func and Expression test

---
+++ CastTest/CastTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CastTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int> myFunc = () => 10;
+            Expression<Func<int>> myExpr = () => 10;
+
+            object[] doubles = { 1.0, 2.0, 3.0 };
+            IEnumerable<double> d = doubles.Cast<double>();
+            var e = d.Where(c => c == 3);
+            WriteLine(e);
+            ReadKey();
+        }
+    }
+}
