dfc2e165f17854647093e5cfa3ceae7cc3b6b08f
503212497 <peter.centellini@cytiva.com>
2021-03-25 10:44:24 +0100

    WinXMenuEditor - Initial

---
+++ WinXMenuEditorTest/WinXMenuEditorTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using MenuWinX;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace WinXMenuEditorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var myWinXLnk = @"C:\Users\503212497\AppData\Local\Microsoft\Windows\WinX\Group4\Paint.lnk";
+            var myWinXApp = @"C:\Windows\regedit.exe";
+            //var myWinXApp = @"C:\Users\503212497\Downloads\WinXMenuEditorRelease\WinXEditor.exe";
+            var myWinXAppDir = @"Path.GetDirectoryName(myWinXApp)";
+            //var myWinXLnk = @"C:\Users\503212497\AppData\Local\Microsoft\Windows\WinX\Group4\Registry Editor.lnk";
+
+            HashLnk.CreateLnk(4, "Registry Editor", myWinXApp, myWinXAppDir, false, "");
+            //HashLnk.CreateLnk(4, "WinX Menu Editor", myWinXApp, myWinXAppDir, true, "");
+            //new HashLnk(myWinXLnk);
+
+            ReadKey();
+        }
+    }
+}
