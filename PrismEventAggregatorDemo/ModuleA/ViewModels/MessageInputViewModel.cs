01e4a7662db9f2fa3c15061507a79bffa86f397a
Peter Centellini <peter.centellini@cytiva.com>
2021-04-02 19:20:12 +0200

    PrismEventAggregatorDemo: Initial commit. Responding to Events

---
+++ PrismEventAggregatorDemo/ModuleA/ViewModels/MessageInputViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using PrismEventAggregatorDemo.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModuleA.ViewModels
+{
+    public class MessageInputViewModel : BindableBase
+    {
+        public DelegateCommand SendMessageCommand { get; private set; }
+
+        private string _message = "Message to Send";
+        private readonly IEventAggregator _eventAggregator;
+
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
+        public MessageInputViewModel(IEventAggregator eventAggregator)
+        {
+            SendMessageCommand = new DelegateCommand(SendMessage);
+            _eventAggregator = eventAggregator;
+        }
+
+        private void SendMessage()
+        {
+            _eventAggregator.GetEvent<MessageSentEvent>()
+                .Publish(Message);
+        }
+    }
+}
---
+++ PrismEventAggregatorDemo/ModuleB/ViewModels/MessageListViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using PrismEventAggregatorDemo.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ModuleB.ViewModels
+{
+    public class MessageListViewModel : BindableBase
+    {
+        private ObservableCollection<string> _messages = new ObservableCollection<string>();
+
+        public ObservableCollection<string> Messages
+        {
+            get { return _messages; }
+            set { SetProperty(ref _messages, value); }
+        }
+
+        public MessageListViewModel(IEventAggregator eventAggregator)
+        {
+            eventAggregator.GetEvent<MessageSentEvent>()
+                .Subscribe(OnMessageReceived);
+        }
+
+        private void OnMessageReceived(string message)
+        {
+            Messages.Add(message);
+        }
+    }
+}
