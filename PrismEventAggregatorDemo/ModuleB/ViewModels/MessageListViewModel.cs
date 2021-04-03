9ef7d26fab795b6b0a6c619e83a9a34a5b5d95c4
Peter Centellini <peter.centellini@cytiva.com>
2021-04-03 19:04:16 +0200

    PrismEventAggregatorDemo: Added Subscribe/Unsubscribe with Delegate method

--- PrismEventAggregatorDemo/ModuleB/ViewModels/MessageListViewModel.cs
+++ PrismEventAggregatorDemo/ModuleB/ViewModels/MessageListViewModel.cs
@@ -11,18 +11,46 @@ namespace ModuleB.ViewModels
 {
     public class MessageListViewModel : BindableBase
     {
-        private ObservableCollection<string> _messages = new ObservableCollection<string>();
+        private MessageSentEvent _event;
 
+        private ObservableCollection<string> _messages = new ObservableCollection<string>();
         public ObservableCollection<string> Messages
         {
             get { return _messages; }
             set { SetProperty(ref _messages, value); }
         }
 
+        private bool _isSubscribed; // = true;
+        public bool IsSubscribed
+        {
+            get { return _isSubscribed; }
+            set 
+            { 
+                SetProperty(ref _isSubscribed, value);
+
+                HandleSubsribe(_isSubscribed);
+            }
+        }
+
+        private void HandleSubsribe(bool isSubscribed)
+        {
+            if (isSubscribed)
+            {
+                _event.Subscribe(OnMessageReceived);
+            }
+            else
+            {
+                _event.Unsubscribe(OnMessageReceived);
+            }
+        }
+
         public MessageListViewModel(IEventAggregator eventAggregator)
         {
-            eventAggregator.GetEvent<MessageSentEvent>()
-                .Subscribe(OnMessageReceived, ThreadOption.PublisherThread, false, message => message.Contains("Peter"));
+            _event = eventAggregator.GetEvent<MessageSentEvent>();
+            //.Subscribe(OnMessageReceived, ThreadOption.PublisherThread, false, message => message.Contains("Peter"));
+
+            //HandleSubsribe(true);
+            IsSubscribed = true;
         }
 
         private void OnMessageReceived(string message)
