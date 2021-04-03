6f81c9862c0e496ade413a8b13596b2105447f16
Peter Centellini <peter.centellini@cytiva.com>
2021-04-03 19:31:55 +0200

    PrismRegionNavigationDemo: Initial Commit

---
+++ PrismRegionNavigationDemo/PrismRegionNavigationDemo/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using PrismRegionNavigationDemo.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace PrismRegionNavigationDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
