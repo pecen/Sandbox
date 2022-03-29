827e4f76e6717128872d52b5a130dd0c890c4065
Peter Centellini <peter.centellini@cytiva.com>
2022-03-29 17:24:57 +0200

    #SimpleIoC: Short demonstration of Dependency Injection

---
+++ SimpleIoC/SimpleIoC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SimpleIoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleIoC ioc = new SimpleIoC();
+
+            ioc.Register<ICustomer, Customer>();
+            ioc.Register<ICustomerRepository, CustomerRepository>();
+            ioc.Register<IDbGateway, DbGateway>();
+            ioc.Register<MainViewModel, MainViewModel>();
+
+            var mainViewModel = ioc.Resolve<MainViewModel>();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SimpleIoC/SimpleIoC/ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIoC
+{
+    public interface ICustomer
+    {
+    }
+
+    public class Customer : ICustomer
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public Customer(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+    }
+
+    public class DbGateway : IDbGateway
+    {
+    }
+
+    public interface IDbGateway
+    {
+    }
+
+    class CustomerRepository : ICustomerRepository
+    {
+        private IDbGateway gateway;
+
+        public CustomerRepository(IDbGateway gateway)
+        {
+            this.gateway = gateway;
+        }
+    }
+
+    public interface ICustomerRepository
+    {
+    }
+
+    public class MainViewModel
+    {
+        public ICustomer Customer { get; set; }
+
+        public MainViewModel(ICustomer customer)
+        {
+            Customer = customer;
+        }
+    }
+}
